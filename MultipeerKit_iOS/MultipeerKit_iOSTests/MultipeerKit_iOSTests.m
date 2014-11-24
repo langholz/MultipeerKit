ef5d9095698d9a8471862bd08b6939a7b0870124
Keith Ermel <keith.ermel@microsoft.com>
2014-11-24 12:17:16 -0800

    Version 0.0.1

---
+++ MultipeerKit_iOS/MultipeerKit_iOSTests/MultipeerKit_iOSTests.m
@@ -0,0 +1,40 @@
+//
+//  MultipeerKit_iOSTests.m
+//  MultipeerKit_iOSTests
+//
+//  Created by Keith Ermel on 11/6/14.
+//  Copyright (c) 2014 Keith Ermel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MultipeerKit_iOSTests : XCTestCase
+
+@end
+
+@implementation MultipeerKit_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
